c2b39537f379d246fdb2c70ae1fb491f914713b3
Dario Miceli <dariomiceli@gmail.com>
2020-06-25 23:27:08 -0700

    fixed modals

--- Flask.py
+++ Flask.py
@@ -17,9 +17,13 @@ def home():
 
 @app.route("/nyc")
 def weatherNYC():
-    url = 'https://avwx.rest/api/multi/metar/KJFK,KLGA,KEWR,kISP,KFRG,KBDR,KDXR,KGON,KPOU,KFOK?token=' + apiKey
-    res = requests.get(url).text
-    goodData = json.loads(res, object_pairs_hook=OrderedDict)
+    METAR_API_CALL = 'https://avwx.rest/api/multi/metar/KJFK,KLGA,KEWR,kISP,KFRG,KBDR,KDXR,KGON,KPOU,KFOK?token=' + apiKey
+    metarRes = requests.get(METAR_API_CALL).text
+    metar = json.loads(metarRes, object_pairs_hook=OrderedDict)
+
+    TAF_API_CALL = 'https://avwx.rest/api/multi/taf/KJFK,KLGA,KEWR,kISP,KFRG,KBDR,KDXR,KGON,KPOU,KFOK?token=' + apiKey
+    tafRes = requests.get(TAF_API_CALL).text
+    tafRes = json.loads(tafRes, object_pairs_hook=OrderedDict)
 
     timeNow = datetime.datetime.now(tz=pytz.UTC)
     timeNYC = timeNow.astimezone(pytz.timezone('US/Eastern')).strftime('%a, %d of %b %H:%M')
@@ -27,7 +31,7 @@ def weatherNYC():
     timeOfDay = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%H:%M')
     dayOfMonth = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%d')
 
-    return render_template("weather.html", data=goodData, timeNYC=timeNYC, timeUTC=timeUTC, dayOfMonth=dayOfMonth,
+    return render_template("weather.html", data=metar,tafRes=tafRes, timeNYC=timeNYC, timeUTC=timeUTC, dayOfMonth=dayOfMonth,
                            timeOfDay=timeOfDay)
 
 
@@ -37,14 +41,18 @@ def weatherLA():
     res = requests.get(url).text
     goodData = json.loads(res, object_pairs_hook=OrderedDict)
 
+    TAF_API_CALL = 'https://avwx.rest/api/multi/taf/KLAX,KVNY,KONT,KSNA,KBUR,KWHP,KOXR,KSBA,KSMX?token=' + apiKey
+    tafRes = requests.get(TAF_API_CALL).text
+    tafRes = json.loads(tafRes, object_pairs_hook=OrderedDict)
+
     timeNow = datetime.datetime.now(tz=pytz.UTC)
     timeNYC = timeNow.astimezone(pytz.timezone('US/Eastern')).strftime('%a, %d of %b %H:%M')
     timeUTC = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%a, %d of %b %H:%M')
     timeOfDay = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%H:%M')
     dayOfMonth = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%d')
 
-    return render_template("weather.html", data=goodData, timeNYC=timeNYC, timeUTC=timeUTC, dayOfMonth=dayOfMonth,
-                           timeOfDay=timeOfDay)
+    return render_template("weather.html", data=goodData, tafRes=tafRes, timeNYC=timeNYC, timeUTC=timeUTC,
+                           dayOfMonth=dayOfMonth, timeOfDay=timeOfDay)
 
 
 @app.route("/sea_tac")
@@ -53,14 +61,18 @@ def weatherSEA():
     res = requests.get(url).text
     goodData = json.loads(res, object_pairs_hook=OrderedDict)
 
+    TAF_API_CALL = 'https://avwx.rest/api/multi/taf/KSEA,KGEG,KBLI,KPSC,KYKM,KPUW,KEAT,KALW,KBFI,KFHR?token=' + apiKey
+    tafRes = requests.get(TAF_API_CALL).text
+    tafRes = json.loads(tafRes, object_pairs_hook=OrderedDict)
+
     timeNow = datetime.datetime.now(tz=pytz.UTC)
     timeNYC = timeNow.astimezone(pytz.timezone('US/Eastern')).strftime('%a, %d of %b %H:%M')
     timeUTC = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%a, %d of %b %H:%M')
     timeOfDay = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%H:%M')
     dayOfMonth = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%d')
 
-    return render_template("weather.html", data=goodData, timeNYC=timeNYC, timeUTC=timeUTC, dayOfMonth=dayOfMonth,
-                           timeOfDay=timeOfDay)
+    return render_template("weather.html", data=goodData, tafRes=tafRes, timeNYC=timeNYC, timeUTC=timeUTC,
+                           dayOfMonth=dayOfMonth, timeOfDay=timeOfDay)
 
 
 @app.route("/dfw")
@@ -69,14 +81,18 @@ def weatherDFW():
     res = requests.get(url).text
     goodData = json.loads(res, object_pairs_hook=OrderedDict)
 
+    TAF_API_CALL = 'https://avwx.rest/api/multi/taf/KSEA,KGEG,KBLI,KPSC,KYKM,KPUW,KEAT,KALW,KBFI,KFHR?token=' + apiKey
+    tafRes = requests.get(TAF_API_CALL).text
+    tafRes = json.loads(tafRes, object_pairs_hook=OrderedDict)
+
     timeNow = datetime.datetime.now(tz=pytz.UTC)
     timeNYC = timeNow.astimezone(pytz.timezone('US/Eastern')).strftime('%a, %d of %b %H:%M')
     timeUTC = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%a, %d of %b %H:%M')
     timeOfDay = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%H:%M')
     dayOfMonth = timeNow.astimezone(pytz.timezone('Zulu')).strftime('%d')
 
-    return render_template("weather.html", data=goodData, timeNYC=timeNYC, timeUTC=timeUTC, dayOfMonth=dayOfMonth,
-                           timeOfDay=timeOfDay)
+    return render_template("weather.html", data=goodData, tafRes=tafRes, timeNYC=timeNYC, timeUTC=timeUTC,
+                           dayOfMonth=dayOfMonth, timeOfDay=timeOfDay)
 
 
 @app.route("/about")
